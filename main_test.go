3342bf557a8fbf137ac3248c77d6662b5582c426
Olga Sokolova <sokolova.o.yu@gmail.com>
2026-02-02 15:52:26 +0300

    first commit>

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"strings"
 	"testing"
 
@@ -46,5 +47,75 @@ func TestCafeWhenOk(t *testing.T) {
 		handler.ServeHTTP(response, req)
 
 		assert.Equal(t, http.StatusOK, response.Code)
+	}
+}
+
+func TestCafeCount(t *testing.T) { // проверяет работу сервера при разных значениях параметра count
+	city := "moscow"
+	r := "/cafe?city=moscow&count="
+	requests := []struct {
+		count int // передаваемое значение count
+		want  int // ожидаемое количество кафе в ответе
+	}{
+		{0, 0},
+		{1, 1},
+		{2, 2},
+		{100, len(cafeList[city])},
+	}
+	handler := http.HandlerFunc(mainHandle)
+
+	for _, v := range requests {
+		response := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", r+strconv.Itoa(v.count), nil)
+		handler.ServeHTTP(response, req)
+
+		assert.Equal(t, http.StatusOK, response.Code)
+		s := strings.TrimSpace(response.Body.String())
+		sep := ","
+		result := strings.Split(s, sep)
+		filteredResult := make([]string, 0, len(result))
+		for _, str := range result {
+			if str != "" {
+				filteredResult = append(filteredResult, str)
+			}
+		}
+		l := len(filteredResult)
+		assert.Equal(t, v.want, l)
+
+	}
+
+}
+
+func TestCafeSearch(t *testing.T) {
+	r := "/cafe?city=moscow&"
+	requests := []struct {
+		search string // передаваемое значение count
+		want   int    // ожидаемое количество кафе в ответе
+	}{
+		{"search=фасоль", 0},
+		{"search=кофе", 2},
+		{"search=вилка", 1},
+	}
+
+	handler := http.HandlerFunc(mainHandle)
+
+	for _, v := range requests {
+		response := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", r+v.search, nil)
+		handler.ServeHTTP(response, req)
+
+		assert.Equal(t, http.StatusOK, response.Code)
+		s := strings.TrimSpace(response.Body.String())
+		sep := ","
+		result := strings.Split(s, sep)
+		filteredResult := make([]string, 0, len(result))
+		for _, str := range result {
+			if str != "" {
+				filteredResult = append(filteredResult, str)
+			}
+		}
+		l := len(filteredResult)
+		assert.Equal(t, v.want, l)
+
 	}
 }
